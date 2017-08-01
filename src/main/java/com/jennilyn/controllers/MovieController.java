e7f73897f6a69baedcb6600036828b3b38df166e
Jennilyn Howell <jennilynhowell@me.com>
2017-08-01 14:00:21 -0400

    able to display index

--- src/main/java/com/jennilyn/controllers/MovieController.java
+++ src/main/java/com/jennilyn/controllers/MovieController.java
@@ -1,7 +1,13 @@
 package com.jennilyn.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MovieController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
 }
